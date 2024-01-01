7b9ea5d40bdc52eb7dfa98367cea1cce39753bcb
SamimSn <samim83sanaee@gmail.com>
2024-01-01 15:52:32 +0330

    added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Django Project README
+
+## Third-Party Apps
+
+- Django==4.2.7
+- django-crispy-forms==2.1
+- django-extensions==3.2.3
+- django-taggit==5.0.1
+- djangorestframework==3.14.0
+- social-auth-app-django==5.4.0
---
+++ generate_readme.py
@@ -0,0 +1,14 @@
+import subprocess
+
+# Run pip freeze command to get installed packages
+output = subprocess.check_output(['pip', 'freeze']).decode('utf-8')
+
+# Filter out only the Django-related packages
+django_packages = [line.strip() for line in output.split('\n') if 'django' in line.lower()]
+
+# Write the Django packages to README.md
+with open('README.md', 'w') as readme_file:
+    readme_file.write('# Django Project README\n\n')
+    readme_file.write('## Third-Party Apps\n\n')
+    for package in django_packages:
+        readme_file.write(f'- {package}\n')
