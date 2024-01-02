2b795bd20d61a9c5862315194d9c9960334e5f7d
SamimSn <samim83sanaee@gmail.com>
2024-01-02 20:03:51 +0330

    like-in-progress

--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponseRedirect
 from .owner import OwnerCreateView, OwnerUpdateView, OwnerDeleteView, OwnerListView, OwnerDetailView, CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView, CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.views import View
+from django.urls import reverse, reverse_lazy
 from .models import Twitt
 
 # Create your views here.
@@ -52,3 +56,27 @@ class UserTwittsListView(OwnerListView):
         context = super(UserTwittsListView, self).get_context_data(**kwargs)        
         context['user_twitts_list'] = True               
         return context  
+    
+    
+def twitt_like(request):    
+    post = get_object_or_404(Twitt, id=request.POST.get('twitt_id'))
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)  
+    return redirect(reverse_lazy('twitter:list'))     
+    
+class TwittLikeView(DetailView):
+    model = Twitt    
+    
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        
+        likes_connected = get_object_or_404(Twitt, id=self.kwargs['pk'])
+        liked = False
+        if likes_connected.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        data['number_of_likes'] = likes_connected.number_of_likes()
+        data['post_is_liked'] = liked
+        return data
+    
